8aa26c97c3617f4208da77b01a301e7e7757361a
squilliams <bimoaryo@rocketmail.com>
2016-05-30 10:11:46 +0700

    Header bisa di highlight :')

--- js/main.js
+++ js/main.js
@@ -13,7 +13,6 @@ jQuery(document).ready(function() {
     nav_container.waypoint({
     handler: function(event, direction) {
         nav.toggleClass('stuck-sticky')
-        console.log('Masuk')
         if (direction == 'down')
           nav_container.css({ 'height':nav.outerHeight() });
         else
@@ -21,15 +20,69 @@ jQuery(document).ready(function() {
     }
     });
     
+    var menu_item = $("#overview");
+    var li = $("ul li.main-menu");
+    
+    menu_item.waypoint({
+    handler: function(event, direction) {
+        li.toggleClass('current')
+    }
+    });
     // Init Skrollr
     var s = skrollr.init({
       smoothScrolling: false,
       mobileDeceleration: 0.004, 
     });
     
-    $('.main-menu a').click(function(){
-      $.scrollTo( this.hash, 1500, { easing:'swing' });
-      return false;
+//      var sections = $('section');
+//      var navigation_links = $('nav ul.main-menu li a');
+//
+//      sections.waypoint({
+//        handler: function(event, direction) {
+//              var active_section;
+//              active_section = $(this);
+//              console.log(this.tagName);
+//                console.log("yes masuk");
+//              if (direction === "up") active_section = active_section.prev();
+//                
+////              var active_link = $('.nav a[href="#' + active_section.attr("id") + '"]');
+//              var active_link = $('nav ul.main-menu li a[href="#' + active_section.attr("id") + '"]');
+//              navigation_links.removeClass("selected");
+//              active_link.addClass("selected");
+//        },
+//        offset: '35%'
+//      });
+    var aChildren = $("nav ul.main-menu li").children(); // find the a children of the list items
+    var aArray = []; // create the empty aArray
+    for (var i=0; i < aChildren.length; i++) {    
+        var aChild = aChildren[i];
+        var ahref = $(aChild).attr('href');
+        aArray.push(ahref);
+    } // this for loop fills the aArray with attribute href values
+
+    $(window).scroll(function(){
+        var windowPos = $(window).scrollTop(); // get the offset of the window from the top of page
+        var windowHeight = $(window).height(); // get the height of the window
+        var docHeight = $(document).height();
+
+        for (var i=0; i < aArray.length; i++) {
+            var theID = aArray[i];
+            var divPos = $(theID).offset().top; // get the offset of the div from the top of page
+            var divHeight = $(theID).height(); // get the height of the div in question
+            if (windowPos >= divPos && windowPos < (divPos + divHeight)) {
+                $("a[href='" + theID + "']").addClass("selected");
+            } else {
+                $("a[href='" + theID + "']").removeClass("selected");
+            }
+        }
+
+        if(windowPos + windowHeight == docHeight) {
+            if (!$("nav ul.main-menu li:last-child a").hasClass("selected")) {
+                var navActiveCurrent = $(".selected").attr("href");
+                $("a[href='" + navActiveCurrent + "']").removeClass("selected");
+                $("nav ul.main-menu li:last-child a").addClass("selected");
+            }
+        }
     });
 });
 
